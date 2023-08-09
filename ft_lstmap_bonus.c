5a16502e05ce4fba02ab51db950ec6dda694d2c8
Felipe Caldas Marques Nasser <fcaldas-@c3r4p2.42sp.org.br>
2023-08-09 20:01:58 -0300

    conclusao v1

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fcaldas- <fcaldas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 17:33:51 by fcaldas-          #+#    #+#             */
-/*   Updated: 2023/08/08 17:34:35 by fcaldas-         ###   ########.fr       */
+/*   Updated: 2023/08/09 19:58:18 by fcaldas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*first;
+	t_list	*new;
+
+	first = 0;
+	if (lst && f && del)
+	{
+		while (lst)
+		{
+			new = ft_lstnew((*f)(lst->content));
+			if (!new)
+			{
+				ft_lstclear(&first, del);
+				return (0);
+			}
+			ft_lstadd_back(&first, new);
+			lst = lst->next;
+		}
+		return (first);
+	}
+	return (0);
 }
